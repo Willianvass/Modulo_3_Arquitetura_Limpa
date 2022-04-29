7eff55709c192547c971c7e937340578c1d70d50
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-04-28 22:39:01 -0300

    Add files via upload

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,109 @@
+﻿using DevFreela.API.Models;
+using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        // api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            var projects = _projectService.GetAll(query);
+
+            return Ok(projects);
+        }
+
+        // api/projects/2
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var project = _projectService.GetById(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] NewProjectInputModel inputModel)
+        {
+            if (inputModel.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            var id = _projectService.Create(inputModel);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, inputModel);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectInputModel inputModel)
+        {
+            if (inputModel.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            _projectService.Update(inputModel);
+
+            return NoContent();
+        }
+
+        // api/projects/3 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _projectService.Delete(id);
+
+            return NoContent();
+        }
+
+        // api/projects/1/comments POST
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentInputModel inputModel)
+        {
+            _projectService.CreateComment(inputModel);
+
+            return NoContent();
+        }
+
+        // api/projects/1/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            _projectService.Start(id);
+
+            return NoContent();
+        }
+
+        // api/projects/1/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            _projectService.Finish(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using DevFreela.API.Models;
+using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // api/users/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userService.GetUser(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // api/users
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel inputModel)
+        {
+            var id = _userService.Create(inputModel);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, inputModel);
+        }
+
+        // api/users/1/login
+        [HttpPut("{id}/login")]
+        public IActionResult Login(int id, [FromBody] LoginModel login)
+        {
+            // TODO: Para Módulo de Autenticação e Autorização
+
+            return NoContent();
+        }
+    }
+}
